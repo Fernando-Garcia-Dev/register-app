68f17b50dedb2fae73565a58c362071a7eea6bce
Fernando Garcia <dfernandorgarcia@gmail.com>
2025-10-14 21:17:09 -0600

    Se levanto un servidor basico con express() :D

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log('Hola mundo :D !!');
+import express from 'express';
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto: ${PORT}`);
+});
