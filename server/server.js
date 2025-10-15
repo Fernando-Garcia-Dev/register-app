655d1b88cf525ff592de73a05ef9acc7fdfff1ee
Fernando Garcia <dfernandorgarcia@gmail.com>
2025-10-14 22:55:22 -0600

    Se sirvió la carpeta public/ y se empezo a diseñar el formulario de registro :D

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
 const PORT = process.env.PORT ?? 3000;
+const path = process.cwd();
 const app = express();
+app.use(express.static(`${path}/public`));
 
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en el puerto: ${PORT}`);
