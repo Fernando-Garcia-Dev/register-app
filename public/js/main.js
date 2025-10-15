4663704313a5fbbb613e3924b38693054de64fb4
Fernando Garcia <dfernandorgarcia@gmail.com>
2025-10-14 23:09:47 -0600

    Se extraen los datos del formulario y se añaden a un objeto listos para ser enviados a el servidor... Investigando como hacer eso :D

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-console.log('Works :D');
+const registerForm = document.querySelector("#form-register");
+
+registerForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const inputs = this.querySelectorAll("input");
+    const dataInfoSend = {};
+    
+    inputs.forEach(input => {
+        const clave = input.name;
+        const valor = input.value;
+        dataInfoSend[clave] = valor;
+    });
+
+    console.log(dataInfoSend)
+
+})
